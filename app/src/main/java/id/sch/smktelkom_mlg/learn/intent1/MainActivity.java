7fa9572e5779fb6ab30b04fcbd56a65bcae1173f
jayanacitra99 <jayana_putra_24rpl@student.smktelkom-mlg.sch.id>
2016-11-05 14:55:16 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String NAMA = "Nama";
+    public static final String UMUR = "Umur";
     Button btnHitung;
+    EditText etNama, etUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnHitung = (Button) findViewById(R.id.buttonHitung);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
+
+        setTitle("Input");
 
         findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HasilActivity.class));
+                goHasil();
             }
         });
+    }
+
+    private void goHasil() {
+        String nama = etNama.getText().toString();
+        String umurStr = etUmur.getText().toString();
+
+        int umur = umurStr.isEmpty() ? 0 : Integer.parseInt(umurStr);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        intent.putExtra(NAMA, nama);
+        intent.putExtra(UMUR, umur);
+
+        startActivity(intent);
     }
 }
